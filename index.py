beb239440c24000509e1d86f41cd88975b28d967
Kevin Luzbetak <luzbetak5739@gmail.com>
2023-06-30 11:44:22 -0400

    mozart

--- index.py
+++ index.py
@@ -30,30 +30,22 @@ if "action" in query:
     elif action == "stop":
         subprocess.run(["mpc", "stop"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print("<p>Music Player Stopped!</p>")
-    elif action == "hitradio":
-        radio_action("http://orf-live.ors-shoutcast.at/oe3-q2a", "Hitradio O3")
-    elif action == "krone":
-        radio_action("http://raj.krone.at/kronehit-ultra-hd.aac", "Krone Hit")
-    elif action == "viva":
-        radio_action("https://stream.sepia.sk:8000/viva320.mp3", "Viva Slovakia")
-    elif action == "fun":
-        radio_action("http://stream.funradio.sk:8000/fun128.mp3", "Fun Radio Slovakia")
-    elif action == "jemne":
-        radio_action("https://stream.bauermedia.sk/melody-hi.mp3", "Slovakia Radio Jemne")
-    elif action == "psyradio":
-        radio_action("http://komplex2.psyradio.org:8010/stream/1/", "PSY Radio")
-    elif action == "psyndora":
-        radio_action("https://cast.magicstreams.gr:9111/stream/1/", "Psyndora Psytrance")
-    elif action == "bucharest":
-        radio_action("http://62.210.105.16:7000/stream/1/", "Bucharest - Deep House")
-    elif action == "nature":
-        radio_action("http://mpc2.mediacp.eu:8376/stream/1/", "Ambient Nature Sleep")
-    elif action == "chillout":
-        radio_action("http://2.58.194.54:8840/stream/1/", "Ambient Chillout Essentials")
+    elif action == "hitradio": radio_action("http://orf-live.ors-shoutcast.at/oe3-q2a", "Hitradio O3")
+    elif action == "krone": radio_action("http://raj.krone.at/kronehit-ultra-hd.aac", "Krone Hit")
+    elif action == "viva": radio_action("https://stream.sepia.sk:8000/viva320.mp3", "Viva Slovakia")
+    elif action == "fun": radio_action("http://stream.funradio.sk:8000/fun128.mp3", "Fun Radio Slovakia")
+    elif action == "jemne": radio_action("https://stream.bauermedia.sk/melody-hi.mp3", "Slovakia Radio Jemne")
+    elif action == "psyradio": radio_action("http://komplex2.psyradio.org:8010/stream/1/", "PSY Radio")
+    elif action == "psyndora": radio_action("https://cast.magicstreams.gr:9111/stream/1/", "Psyndora Psytrance")
+    elif action == "bucharest": radio_action("http://62.210.105.16:7000/stream/1/", "Bucharest - Deep House")
+    elif action == "nature": radio_action("http://mpc2.mediacp.eu:8376/stream/1/", "Ambient Nature Sleep")
+    elif action == "chillout": radio_action("http://2.58.194.54:8840/stream/1/", "Ambient Chillout Essentials")
+    elif action == "mozart": radio_action("http://37.251.146.169:8300/stream/1/", "Classic Mozart")
 
 menu = [
     {"link": "?action=start", "name": "Start Music Player"},
     {"link": "?action=stop", "name": "Stop Music Player"},
+    {"link": "?action=mozart", "name": "Classic Mozart"},
     {"link": "?action=hitradio", "name": "Austria Hitradio O3"},
     {"link": "?action=krone", "name": "Austria Krone Hit"},
     {"link": "?action=viva", "name": "Slovakia Viva"},
